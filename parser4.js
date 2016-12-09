e858267fbb468076b5480881afbe3397375ec055
TomStuart92 <Thomascstuart@gmail.com>
2016-12-09 22:42:49 +0000

    Add File Persistence

--- parser4.js
+++ parser4.js
@@ -2,13 +2,17 @@
 var Converter = require("csvtojson").Converter;
 var fs = require('fs');
 
-var converter = new Converter({});
-converter.fromFile("./exerciseLog.csv",function(err,result){
-  fs.writeFile("./tmp/data.json", result, function(err) {
-      if(err) {
-          return console.log(err);
-      }
-
-      console.log("The file was saved!");
-  });
-});
+module.exports = {
+  loadFromCSVFile: function(){
+    var converter = new Converter({});
+    converter.fromFile("./exerciseLog.csv",function(err,result){
+      string = JSON.stringify({data: result});
+      fs.writeFile("./tmp/data.json", string, function(err) {
+          if(err) {
+              return console.log(err);
+          }
+          console.log("The file was saved!");
+      });
+    });
+  }
+}
