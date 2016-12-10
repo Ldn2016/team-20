993a6cfd9a4528323af6d41426ccec16f2ca19c7
TomStuart92 <Thomascstuart@gmail.com>
2016-12-10 07:18:32 +0000

    Hook Up Interface to Backend

--- index.js
+++ index.js
@@ -5,49 +5,49 @@ var nodeFinder = require('./lib/nodeFinder.js');
 var intial = require('./data/initial.js')
 
 this.server = http.createServer(function(req, res) {
-  if (req.url === '/') {
+  if (req.url === '/' && req.method === 'GET') {
     fs.readFile('./views/index.html', {encoding: 'utf8'}, function(err, page){
       res.writeHead(200);
       res.end(page);
     })
-  } else if (req.url === '/nodes/new') {
+  } else if (req.url === '/nodes/new'&& req.method === 'POST') {
     var whole = "";
-    res.on("data", (chunk) => {whole += chunk.toString()});
-    res.on("end", () => {
+    req.on("data", (chunk) => {whole += chunk.toString()});
+    req.on("end", () => {
       var updatedHistory = nodeFinder.updateWithNextNode(whole);
-      req.writeHead(200);
-      req.end(updatedHistory)
+      res.writeHead(200);
+      res.end(JSON.stringify(updatedHistory));
     })
-  } else if  (req.url === '/public/application.js') {
+  } else if  (req.url === '/public/application.js' && req.method === 'GET') {
     fs.readFile('./public/application.js', {encoding: 'utf8'}, function(err, page){
       res.writeHead(200, {'Content-Type': 'application/javascript'});
       res.end(page);
     });
-  } else if  (req.url === '/public/styles.css') {
+  } else if  (req.url === '/public/styles.css'&& req.method === 'GET') {
     fs.readFile('./public/styles.css', {encoding: 'utf8'}, function(err, page){
       res.writeHead(200, {'Content-Type': 'text/css'});
       res.end(page);
     });
-  } else if  (req.url === '/public/cytoscape.min.js') {
+  } else if  (req.url === '/public/cytoscape.min.js'&& req.method === 'GET') {
     fs.readFile('./public/cytoscape.min.js', {encoding: 'utf8'}, function(err, page){
       res.writeHead(200, {'Content-Type': 'application/javascript'});
       res.end(page);
     });
-  } else if (req.url === '/initial') {
-    res.setHeader('Content-Type', 'application/json');
+  } else if (req.url === '/initial'&& req.method === 'GET') {
+    res.setHeader('Content-Type', 'application/jsonp');
     res.write(JSON.stringify(intial));
       res.end();
-  } else if (req.url === '/img/completed') {
+  } else if (req.url === '/img/completed'&& req.method === 'GET') {
     fs.readFile('./img/completed.png', function(err, img){
       res.writeHead(200);
       res.end(img);
     })
-  } else if (req.url === '/img/locked') {
+  } else if (req.url === '/img/locked'&& req.method === 'GET') {
     fs.readFile('./img/locked.png', function(err, img){
       res.writeHead(200);
       res.end(img);
     })
-  } else if (req.url === '/img/inprogress') {
+  } else if (req.url === '/img/inprogress'&& req.method === 'GET') {
     fs.readFile('./img/inProgress.png', function(err, img){
       res.writeHead(200);
       res.end(img);
