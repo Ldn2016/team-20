db2e2c8baa6581466246d8de3e6b46c98ba120d2
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-12-10 00:35:52 +0000

    deleted some commented code

--- public/index.js
+++ public/index.js
@@ -92,26 +92,6 @@ var cy = cytoscape({
       }),
 
   elements: data,
-  //   nodes: [
-  //     { data: { id: '0' , link: "http://demo.learningequality.org/learn/khan/math/early-math/cc-early-math-counting-topic/cc-early-math-counting/counting-with-small-numbers/" } },
-  //     { data: { id: '1' , link: "http://www.google.com", completed: false } },
-  //     { data: { id: '2' , link: "https://github.com", completed: false } },
-  //     { data: { id: '3' , link: "string0", completed: false } },
-  //     { data: { id: '4' , link: "string0", completed: false } },
-  //     { data: { id: '5' , link: "string0", completed: false } },
-  //     { data: { id: '6' , link: "string0", completed: false } },
-  //     { data: { id: '7' , link: "string0", completed: false } }
-  //   ],
-  //   edges: [
-  //     { data: { source: '0', target: '1' } },
-  //     { data: { source: '1', target: '2' } },
-  //     { data: { source: '2', target: '3' } },
-  //     { data: { source: '3', target: '4' } },
-  //     { data: { source: '4', target: '5' } },
-  //     { data: { source: '5', target: '6' } },
-  //     { data: { source: '6', target: '7' } }
-  //   ]
-  // }
   layout: {
     name: 'breadthfirst',
     directed: true,
