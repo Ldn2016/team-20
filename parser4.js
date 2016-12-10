17d8e2b0af0086ceff95ae4993416ba648d070ed
Tam <tamborinehands@gmail.com>
2016-12-10 08:18:34 +0000

    Fix Merge

---
+++ parser4.js
@@ -0,0 +1,18 @@
+//Converter Class
+var Converter = require("csvtojson").Converter;
+var fs = require('fs');
+
+module.exports = {
+  loadFromCSVFile: function(){
+    var converter = new Converter({});
+    converter.fromFile("./exerciseLog.csv",function(err,result){
+      string = JSON.stringify({data: result});
+      fs.writeFile("./tmp/data.json", string, function(err) {
+          if(err) {
+              return console.log(err);
+          }
+          console.log("The file was saved!");
+      });
+    });
+  }
+}
---
+++ playing.js
@@ -0,0 +1,39 @@
+// {sourcenode:{targetnode: node, weight: n}, sourcenode2: {targetnode: weight}}
+//input paths    {uid:[[exercise, score],[]], uid2:[[],[],[]]}
+
+module.exports = {
+  pathsToGraph: function(paths){
+    var graph = {}
+
+    return graph
+  }
+}
+
+
+newPaths = []
+for uid in paths:
+  for a,b in paths[uid]:
+
+{}
+
+for every path in the object
+  for node, nextnode in the path
+    graph[node] = {nextnode[0]: nextnode[1]} //get ex name and weight of target node and attach to source node
+
+arr = [[A:6],[],[],[],[F:9]]
+
+for(i=0, i < arr.length-1, i++){
+
+}
+
+
+
+// def parse_txt_file(graph_info) "AB8, CD8"
+//    graph_info.scan(/\w\w\d+/).each do |x|
+//      start = x[0,1]
+//      finish = x[1,1]
+//      @graph[start] ||= { }
+//      @graph[start][finish] = x[2..-1].to_i
+//      p @graph
+//    end
+//  end
